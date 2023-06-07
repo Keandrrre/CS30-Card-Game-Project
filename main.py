48d0c9327927bfdd86a48b29942fdbbe7375b13a
Keandrrre <k.kuzniar@share.epsb.ca>
2023-06-07 14:47:36 -0600

    Keyboard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Card Game (War)
 import random
+import os
+import time
+import keyboard
 
 
 # Player Class
@@ -43,14 +46,36 @@ class Deck:
     def shuffle_deck(self):
         random.shuffle(self.cards)
 
+
 # Game Class
 
 
 class Game:
     # Initialize Constructor Method (game)
     def __init__(self, player1, player2):
+        self.deck = Deck
         self.player1 = player1
         self.player2 = player2
 
+    # Methods (game)
+    def deal_cards(self):
+        self.deck.shuffle_deck
+        print('Dealing Cards')
 
-print('check')
+
+while True:
+    # Main Menu
+    os.system('cls')
+    print("WAR\n\nOptions:\n1 -- 'Start New Game'\n2 -- 'Rules'")
+    if keyboard.is_pressed('1'):
+        os.system('cls')
+        # Enter Player Names
+        p1_name = str(input("Enter Player 1 Name: "))
+        p2_name = str(input("Enter Player 2 Name: "))
+        os.system('cls')
+        p1 = Player(p1_name)
+        p2 = Player(p2_name)
+        # Game Starts
+        print('Starting War!')
+        new_game = Game(p1, p2)
+        new_game.deal_cards
